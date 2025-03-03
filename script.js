3cfa88e78b98b325b26a59b043c488e691e3a2f7
ACL-CSS <atorreblanca706@gmail.com>
2025-03-03 17:50:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let fruits = {"Grapes" , "Apples" , "Pears" , "Oranges"};
 let fruit = fruits[0]; //first fruit
+function logfirstFruit()
+{
+  let randomNumber= Math.floor(Math.random()*fruits.length);
+  consol.log(fruits[0]);
+}
+logFirstFruit();
